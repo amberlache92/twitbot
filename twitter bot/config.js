0ea2adb2cbc35e53901365ae4bc358cf4a750d63
Amber Capell <acapell@Corporate.HealthCarousel.com>
2017-07-31 16:36:33 -0400

    inital commit

---
+++ twitter bot/config.js
@@ -0,0 +1,14 @@
+
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'jAzyv5RaaQWeJzQipjKibqhfy ',  
+  consumer_secret: '8z1c85fdRLR2cEiLu0ddiZD3ag48v52CT3KrrJ5eeFoHBhIEKg',
+  access_token: '	892054986485616641-ZzhrFu7uoJHmD65iFGzisFW7sX7Lkij',  
+  access_token_secret: 'XVjTG7fJfi6ad7eA7uvZlenO4OordquwyMlqU8cRoQeqx'
+}
