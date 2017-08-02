3230d16fbd6a034293f80d35946e16c1de1cb3ac
Amber Capell <acapell@Corporate.HealthCarousel.com>
2017-08-02 08:45:30 -0400

    retweets big brother hashtag

--- twitter bot/bot.js
+++ twitter bot/bot.js
@@ -1,4 +1,66 @@
-var twit = require(’twit’);
-var config = require(’./config.js’);
+var twit = require('twit');
+var config = require('./config.js');
+var TWITTER_SEARCH_PHRASE= "#bb19"
+var bot = new twit(config);
+console.log('The bot is running...');
+bot.post('statuses/update', {status: 'I Love Tech Knights!'},  function(error, tweet, response){
+  if(error){
+    console.log(error);
+  }
+  console.log(tweet);  // Tweet body.
+  console.log(response);  // Raw response object.
+});
+/* BotInit() : To initiate the bot */
+function BotInit() {
+	bot.post('statuses/retweet/:id', { id: '669520341815836672' }, BotInitiated);
+	
+	function BotInitiated (error, data, response) {
+		if (error) {
+			console.log('Bot could not be initiated, : ' + error);
+		}
+		else {
+  			console.log('Bot initiated : 669520341815836672');
+		}
+	}
+	
+	BotRetweet();
+}
 
-var Twitter = new twit(config);
+/* BotRetweet() : To retweet the matching recent tweet */
+function BotRetweet() {
+
+	var query = {
+		q: TWITTER_SEARCH_PHRASE,
+		result_type: "recent"
+	}
+
+	bot.get('search/tweets', query, BotGotLatestTweet);
+
+	function BotGotLatestTweet (error, data, response) {
+		if (error) {
+			console.log('Bot could not find latest tweet, : ' + error);
+		}
+		else {
+			var id = {
+				id : data.statuses[0].id_str
+			}
+
+			bot.post('statuses/retweet/:id', id, BotRetweeted);
+			
+			function BotRetweeted(error, response) {
+				if (error) {
+					console.log('Bot could not retweet, : ' + error);
+				}
+				else {
+					console.log('Bot retweeted : ' + id.id);
+				}
+			}
+		}
+	}
+	
+	/* Set an interval of 30 minutes (in microsecondes) */
+	setInterval(BotRetweet, 30*60*1000);
+}
+
+/* Initiate the Bot */
+BotInit();
--- twitter bot/config.js
+++ twitter bot/config.js
@@ -7,8 +7,10 @@
  * access_token_secret
  */
 module.exports = {
-  consumer_key: 'jAzyv5RaaQWeJzQipjKibqhfy ',  
-  consumer_secret: '8z1c85fdRLR2cEiLu0ddiZD3ag48v52CT3KrrJ5eeFoHBhIEKg',
-  access_token: '	892054986485616641-ZzhrFu7uoJHmD65iFGzisFW7sX7Lkij',  
-  access_token_secret: 'XVjTG7fJfi6ad7eA7uvZlenO4OordquwyMlqU8cRoQeqx'
-}
+  consumer_key: 'CtaHwulYLge2CoZGxywkLWMVr',  
+  consumer_secret: 'BHGNMQlHpDpq72Hgcb2kDRXnXun6XMelFWDNr07TAIiqL4tbw1',
+  access_token: '892054986485616641-ZzhrFu7uoJHmD65iFGzisFW7sX7Lkij',  
+  access_token_secret: 'XVjTG7fJfi6ad7eA7uvZlenO4OordquwyMlqU8cRoQeqx',
+ 
+
+	}
