4c84478f30d925f3862666b9300b17a400087f49
Amber  Capell <amberlache92@gmail.com>
2017-08-18 11:24:54 -0400

    Update config.js

--- twitter bot/config.js
+++ twitter bot/config.js
@@ -7,11 +7,11 @@
  * access_token_secret
  */
 module.exports = {
-consumer_key: 'gdQ9nhBAr9RN8IUSIPgN58EBj',
-  consumer_secret: 'kqcYj7mP0trmMFgVnmGHwX3HPvWZUteojcZWXksYOBJCg1ix9x'
+consumer_key: '',
+  consumer_secret: ''
 ,
-  access_token: '892054986485616641-nAExdr7vukmSIwRudHjM4k2YxuzaOHS',
-  access_token_secret:'717jw4s6DBTEcsJalY1s3pkE4wxfXFsoOEs9SDCR7NRxi'
+  access_token: '',
+  access_token_secret:''
 ,
  
 
